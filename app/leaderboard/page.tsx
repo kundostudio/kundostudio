12e630a08cf7ccf9197e1a02c3288ad94f9abb0e
Emanuel Lorenzo <aemalorenzo@gmail.com>
2024-09-05 15:20:58 -0300

    leaderboard page WIP

--- app/leaderboard/page.tsx
+++ app/leaderboard/page.tsx
@@ -1,3 +1,15 @@
+import { Typography } from "~/components/typography";
+
+import styles from "./leaderboard.module.scss";
+
 export default function Page() {
-  return <div>Leaderboard</div>;
+  return (
+    <main className={styles.leaderboardPage}>
+      <Typography.H2 className={styles.title}>leaderboard</Typography.H2>
+      <div className={styles.lineHorizontal} />
+      <div className={styles.leaderboard}></div>
+      <div className={styles.lineHorizontal} />
+      <Typography.H2 className={styles.title}>recent joins</Typography.H2>
+    </main>
+  );
 }
