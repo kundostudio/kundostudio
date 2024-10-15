a684f8d7884537cc13faf951da3a8b34f26f0a40
Emanuel Lorenzo <aemalorenzo@gmail.com>
2024-10-15 16:16:31 -0300

    updated new colors

--- src/constants/themes.ts
+++ src/constants/themes.ts
@@ -3,18 +3,18 @@ import { Theme } from "~/types";
 export const THEMES: Theme[] = [
   {
     name: "stereo",
-    color: "212, 254, 0",
+    color: "172, 253, 90", // #ACFD5A
   },
   {
     name: "grid",
-    color: "255, 255, 255",
+    color: "255, 255, 255", // #FFFFFF
   },
   {
     name: "horizon",
-    color: "128, 233, 255",
+    color: "128, 233, 255", // #80E9FF
   },
   {
     name: "wave",
-    color: "180, 130, 255",
+    color: "180, 130, 255", // #B482FF
   },
 ];
