092c138cc74fbc9319a3ef5502b7d8e4c34d7635
Emanuel Lorenzo <aemalorenzo@gmail.com>
2024-11-05 15:23:03 -0300

    reduce title font size, delete leaderboard title and slowdown recent joins marquee

--- app/leaderboard/page.tsx
+++ app/leaderboard/page.tsx
@@ -15,12 +15,10 @@ export default function Page() {
   const isUpperMobile = useMediaQuery("(min-width: 640px)");
   return (
     <main className={styles.leaderboardPage}>
-      <Typography.H2 className={styles.title}>leaderboard</Typography.H2>
-      <Line direction="horizontal" />
       <Leaderboard className={styles.leaderboardWrapperWrapper} />
       <Line direction="horizontal" />
       <Typography.H2 className={styles.title}>recent joins</Typography.H2>
-      <Marquee className={styles.recentJoinMarquee} duration={60} offset={0}>
+      <Marquee className={styles.recentJoinMarquee} speed={0.5} offset={0} slowDownOnHover>
         {RECENT_JOINS.map(({ name, since }) => (
           <div key={name} className={styles.recentJoin}>
             <span className={styles.recentJoinName}>{name}</span>
