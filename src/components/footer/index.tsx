04e91878f323862e2cba6b1375d08ae886936751
Emanuel Lorenzo <aemalorenzo@gmail.com>
2024-11-05 15:09:00 -0300

    update footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,5 +1,4 @@
 import { Typography } from "~/components/typography";
-import { cn } from "~/lib/utils";
 import M from "~/public/social/m.svg";
 import O from "~/public/social/o.svg";
 import Telegram from "~/public/social/telegram.svg";
@@ -14,23 +13,15 @@ export function Footer() {
   return (
     <footer className={styles.wrapper}>
       <div className={styles.poweredBy}>
-        <Typography.Span className="opacity-40 tracking-[0.08em]">powered by:</Typography.Span>
-        <Typography.Span className="tracking-[0.08em]">
-          Ethereum <span className="opacity-40 tracking-[0.08em]">x</span> Polygon zkevm
+        <Typography.Span className="opacity-40 tracking-[0.08em]">powered by: </Typography.Span>
+        <Typography.Span className="tracking-[0.08em] pl-2">
+          Zero <span className="opacity-40 tracking-[0.08em]">x</span> Z Chain{" "}
+          <span className="opacity-40 tracking-[0.08em]">x</span> Polygon ZKVM
         </Typography.Span>
       </div>
       <div className={styles.prices}>
         <div className="flex items-center gap-2">
-          <Typography.Span className="opacity-40 tracking-[0.08em]">tvl:</Typography.Span>
-          <Typography.Span className="tracking-[0.08em]">$201,101,333</Typography.Span>
-        </div>
-        <Typography.Span
-          className={cn(styles.separator, "leading-3 opacity-30 mr-0 tracking-[0.08em]")}
-        >
-          {"//"}
-        </Typography.Span>
-        <div className="flex items-center gap-2">
-          <Typography.Span className="opacity-40 tracking-[0.08em] mobile:before:content-['//'] mobile:before:pr-2">
+          <Typography.Span className="opacity-40 tracking-[0.08em] mobile:before:pr-2">
             wallets:
           </Typography.Span>
           <Typography.Span className="tracking-[0.08em]">21,369</Typography.Span>
