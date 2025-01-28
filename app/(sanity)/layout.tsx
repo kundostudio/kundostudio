22d73629252a6ae38b8b5513be9c8c48f9613824
Emanuel Lorenzo <aemalorenzo@gmail.com>
2025-01-28 16:01:14 -0300

    moved studio to standalone project

--- app/(sanity)/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function SanityLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
-}
--- schemas/index.ts
+++
@@ -1,5 +0,0 @@
-import project from "./project";
-import quote from "./quote";
-import skill from "./skill";
-
-export const schemaTypes = [project, quote, skill];
