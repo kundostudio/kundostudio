d727f955d7c3c932e3a5b87e7036d2235b0d0b84
Facundo <66272813+facumontanaro@users.noreply.github.com>
2025-05-24 16:39:15 -0300

    Update layout.tsx
    Force dark mode theme always on.

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" className="dark" suppressHydrationWarning>
       <body suppressHydrationWarning>{children}</body>
     </html>
   );
