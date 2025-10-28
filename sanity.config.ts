8ae848e90a5e61f5617a067d18f62d55662b56f0
Emanuel Lorenzo <hello@emalorenzo.com>
2025-10-27 21:13:09 -0300

    fix config file

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,4 +1,5 @@
 import { visionTool } from "@sanity/vision";
+import type { Config } from "sanity";
 import { defineConfig } from "sanity";
 import { presentationTool } from "sanity/presentation";
 import { structureTool } from "sanity/structure";
@@ -6,17 +7,17 @@ import { muxInput } from "sanity-plugin-mux-input";
 
 import { apiVersion, dataset, projectId } from "./src/sanity/env";
 import { schema } from "./src/sanity/schemas";
-
 import { structure } from "./src/sanity/structure";
 
-export default defineConfig({
+const config: Config = defineConfig({
 	name: "kundo-studio",
 	title: "Kundo Studio",
 	projectId,
 	dataset,
 	basePath: "/studio",
 	plugins: [
-		structureTool({ structure }), // Vision is for querying with GROQ from inside the Studio
+		structureTool({ structure }),
+		// Vision is for querying with GROQ from inside the Studio
 		// https://www.sanity.io/docs/the-vision-plugin
 		visionTool({ defaultApiVersion: apiVersion }),
 		presentationTool({
@@ -30,3 +31,5 @@ export default defineConfig({
 	],
 	schema,
 });
+
+export default config;
